bfd79bfa2573dc2d4eb113931021cdb22abdc184
Guillermo Guzmán Sánchez <guguzma@bancolombia.com.co>
2022-02-22 22:53:57 -0500

    Added DataMapper

--- src/main/java/com/github/billy/bar/common/mapper/DataMapper.java
+++ src/main/java/com/github/billy/bar/common/mapper/DataMapper.java
@@ -3,14 +3,21 @@ package com.github.billy.bar.common.mapper;
 import com.github.billy.bar.common.type.Data;
 import org.mapstruct.Mapper;
 
-@Mapper(componentModel = "spring")
-public interface DataMapper {
+import java.util.List;
+import java.util.stream.Collectors;
 
-  default Integer map(Data data) {
-    return data.getValue();
-  }
+@Mapper(componentModel = "spring", imports = {Data.class})
+public interface DataMapper {
 
   default Data map(Integer value) {
     return new Data(value);
+  }
+
+  default List<Integer> toInteger(List<Data> dataList) {
+    return dataList.stream().map(this::map).collect(Collectors.toList());
+  }
+
+  default Integer map(Data data) {
+    return data.getValue();
   }
 }
