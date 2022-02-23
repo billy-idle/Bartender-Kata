6e018be2b75ea2d97c1713b12417849736065388
Guillermo Guzmán Sánchez <guguzma@bancolombia.com.co>
2022-02-23 18:11:35 -0500

    Fixed inspections

--- src/main/java/com/github/billy/bar/common/type/BaseType.java
+++ src/main/java/com/github/billy/bar/common/type/BaseType.java
@@ -1,10 +1,8 @@
 package com.github.billy.bar.common.type;
 
-import java.io.Serial;
 import java.io.Serializable;
 
 public abstract class BaseType implements Serializable {
 
-  @Serial
   private static final long serialVersionUID = 1L;
 }
